a3c42f73f994955ad6b1abc16b99fc89a63e96eb
astarta16 <ngedeshuri2001@gmail.com>
2024-06-29 10:54:55 +0400

    add third

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ let name = "mariam";
 console.log(name == "mariam" ? true : false)
 
 // N3
+let girlName = "mariam";
+
+switch(girlName) {
+    case "mariam":
+        console.log(true);
+        break;
+    default:
+        console.log(false);
+}
+
+   
+
