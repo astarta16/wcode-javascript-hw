32876416f1d7a5eea21fd40f28df891d09941a81
astarta16 <ngedeshuri2001@gmail.com>
2024-07-01 10:40:13 +0400

    add another solved problem

--- index.js
+++ index.js
@@ -59,4 +59,13 @@ for(let i = 0; i <array6.length; i++) {
 
 
 // N8
+let users = [
+    {username: 'giorgi', status: false},
+    {username: 'levani', status: false},
+    {username: 'anna', status: true}
+]
 
+for (let i = 0; i < users.length; i++) {
+    if (users[i].status )
+        console.log(users[i])
+}
